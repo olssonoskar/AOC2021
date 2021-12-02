34d29aae0552f85bc8445c313b8fbe4d21fff6c5
Oskar Olsson <oskar94olsson@gmail.com>
2021-12-02 08:09:43 +0100

    variable name change

--- src/main/java/com/olsson/aoc2021/Day02.java
+++ src/main/java/com/olsson/aoc2021/Day02.java
@@ -8,34 +8,34 @@ public class Day02 implements Solution{
 
     @Override
     public String part1(List<String> input) {
-        var position = new Submarine(0, 0, 0);
+        var sub = new Submarine(0, 0, 0);
         for (var command: input) {
             var directionAndUnits = command.split(SPACE);
             var units = Integer.parseInt(directionAndUnits[1]);
-            position = switch (directionAndUnits[0]) {
-                case "forward" -> position.moveX(units);
-                case "down" -> position.moveY(units);
-                case "up" -> position.moveY(-units);
+            sub = switch (directionAndUnits[0]) {
+                case "forward" -> sub.moveX(units);
+                case "down" -> sub.moveY(units);
+                case "up" -> sub.moveY(-units);
                 default -> throw new IllegalArgumentException("Unknown command: " + directionAndUnits[0]);
             };
         }
-        return String.valueOf(position.x * position.y);
+        return String.valueOf(sub.x * sub.y);
     }
 
     @Override
     public String part2(List<String> input) {
-        var position = new Submarine(0, 0, 0);
+        var sub = new Submarine(0, 0, 0);
         for (var command: input) {
             var directionAndUnits = command.split(SPACE);
             var units = Integer.parseInt(directionAndUnits[1]);
-            position = switch (directionAndUnits[0]) {
-                case "forward" -> position.moveBasedOnAim(units);
-                case "down" -> position.pitch(units);
-                case "up" -> position.pitch(-units);
+            sub = switch (directionAndUnits[0]) {
+                case "forward" -> sub.moveBasedOnAim(units);
+                case "down" -> sub.pitch(units);
+                case "up" -> sub.pitch(-units);
                 default -> throw new IllegalArgumentException("Unknown command: " + directionAndUnits[0]);
             };
         }
-        return String.valueOf(position.x * position.y);
+        return String.valueOf(sub.x * sub.y);
     }
 
     @Override
